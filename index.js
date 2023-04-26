9e6a002c0ade3047a2991d429cd0bb0abb16fb2d
Eban451 <eban316@gmail.com>
2023-04-26 01:49:41 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,121 @@
+const express = require("express")
+const pg = require("pg")
+const cors = require('cors')
+const app = express()
+const { Pool } = pg;
+app.use(express.json());
+app.use(cors());
+
+const pool = new Pool({
+    host: "localhost",
+    user: "postgres",
+    database: "puntos",
+    password: "12345",
+    port: 5432
+})
+
+app.listen(4000)
+
+app.get("/api/v1/users", async (req, res) => {
+    const resultado = await pool.query("select  * from users order by id");
+    res.json(resultado.rows)
+})
+
+app.get("/api/v1/puntos2", async (req, res) => {
+    const resultado = await pool.query("SELECT id, nombre, img, Direccion, Horario, ST_AsGeoJSON(geom) AS geometry FROM museums order by id");
+    const features = resultado.rows.map(row => ({
+        type: 'Feature',
+        geometry: JSON.parse(row.geometry).coordinates,
+        nombre: row.nombre,
+        img: row.img,
+        direccion: row.direccion,
+        horario: row.horario        
+    }));
+    res.json(features);
+});
+
+app.get("/api/v1/users2", async (req, res) => {
+    const { email } = req.query;
+    const query = email ? `SELECT * FROM users WHERE email = $1` : `SELECT * FROM users ORDER BY id`;
+    const values = email ? [email] : [];
+    const resultado = await pool.query(query, values);
+    res.json(resultado.rows);
+  });
+
+  app.get("/api/v1/users/:id", async (req, res) => {
+    const { id } = req.params;
+    const query = `SELECT * FROM users WHERE id = $1`;
+    const values = [id];
+    const resultado = await pool.query(query, values);
+    res.json(resultado.rows[0]);
+  });
+
+app.get("/api/v1/puntos", async (req, res) => {
+    const resultado = await pool.query(
+        "SELECT id, nombre, img, Direccion, Horario, ST_AsGeoJSON(geom) AS geometry FROM museums order by id"
+    );
+    const geojsonData = {
+        type: "FeatureCollection",
+        features: resultado.rows.map((feature) => ({
+            type: "Feature",
+            geometry: JSON.parse(feature.geometry),
+            properties: {
+                id: feature.id,
+                nombre: feature.nombre,
+                img: feature.img,
+                Direccion: feature.direccion,
+                Horario: feature.horario,
+            },
+        })),
+    };
+    res.json(geojsonData);
+});
+
+app.delete("/api/v1/users/:id", async (req, res) => {
+    try {
+        const { id } = req.params
+        const resultado = await pool.query("delete from users where id=$1 RETURNING id", [id]);
+        if (resultado.rows) {
+            res.status(200).json({ id: resultado.rows[0].id })
+        } else {
+            res.status(404).json({ error: "Registro no Existe" })
+        }
+    } catch (e) {
+        res.status(500).json({ error: e })
+    }
+
+
+})
+
+// INGRESAR DATOS
+
+app.post("/api/v1/users", async (req, res) => {
+    const { name, email, password } = req.body;
+    const resultado = await pool.query(
+      "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id",
+      [name, email, password]
+    );
+    console.log(resultado);
+    res.json({});
+  });
+
+
+// PRUEBA REGISTRO
+
+app.post("/api/v1/users", async (req, res) => {
+    const { name, email, password } = req.body;
+    const resultado = await pool.query("INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id", [name, email, password]);
+    console.log(resultado);
+    res.json({});
+});
+
+
+// EDITAR
+
+app.put("/api/v1/users/:id", async (req, res) => {
+    const { id, name, email } = req.body;
+    const resultado = await pool.query("update users set name =$1, email=$2 where id=$3", [name, email, id]);
+    console.log(resultado),
+        res.json({})
+
+})
